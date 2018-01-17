889c18392c931eef2ed9c63fd09468ccb96c0b40
lufinkey <luisfinke@gmail.com>
2018-01-17 13:46:36 -0500

    registerNativeModule

--- index.js
+++ index.js
@@ -3,4 +3,19 @@ import { NativeModules } from 'react-native';
 
 const { RNEventEmitter } = NativeModules;
 
-export default RNEventEmitter;
+const moduleIdCounter = 0;
+
+function registerNativeModule(nativeModule)
+{
+	if(!nativeModule.__registerAsJSEventEmitter)
+	{
+		throw new Error("Native module does not conform to RNEventConformer");
+	}
+	var moduleId = moduleIdCounter;
+	moduleIdCounter++;
+	nativeModule.__registerAsJSEventEmitter(moduleId);
+}
+
+export default {
+	registerNativeModule: registerNativeModule
+};
